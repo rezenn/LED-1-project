1a00011e654948e683d633433d969019dfd9a494
sudipchaudhary30 <chsudip30@gmail.com>
2024-02-23 21:51:09 +0545

    Adding function in selecting admin or employee

--- usertype_select.py
+++ usertype_select.py
@@ -2,6 +2,14 @@ from tkinter import *
 from tkinter import Tk
 from PIL import Image, ImageTk
 
+def adminlog():
+    root.destroy()
+    import Loginadmin
+
+def employeelog():
+    root.destroy()
+    import Employeelogin   
+
 root = Tk()
 root.geometry('1280x800')
 root.resizable(0, 0)
@@ -19,10 +27,10 @@ label2.place(x=110,y=200)
 label1=Label(text="Are you an employee and admin",font=("Herlad",17,"bold"),bg="white")
 label1.place(x=115,y=290)
 
-ButtonEmp=Button(text="Employee",font=("Herlad",17,"bold"),height=2,bd=0,bg="grey")
+ButtonEmp=Button(text="Employee",font=("Herlad",17,"bold"),height=2,bd=0,bg="grey",command=employeelog)
 ButtonEmp.place(x=120,y=380)
 
-Buttonadm=Button(text="   Admin    ",font=("Herlad",17,"bold"),height=2,bd=0,bg="grey")
+Buttonadm=Button(text="   Admin    ",font=("Herlad",17,"bold"),height=2,bd=0,bg="grey", command=adminlog)
 Buttonadm.place(x=320,y=380)
 
 
